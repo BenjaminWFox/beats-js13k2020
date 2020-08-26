9a5d15ad32330236a5d4b697e16b2bcba72f93c7
Ben Fox <BenjaminWFox@gmail.com>
2020-08-25 19:37:02 -0700

    SUccessful transition from title screen to first level

--- src/index.js
+++ src/index.js
@@ -69,7 +69,7 @@ let audioReady = undefined
 let loadedLevels = 0
 const levels = [lt, l0, l1, l2, l3, l4, l5]
 
-const beatsInTransit = []
+let beatsInTransit = []
 const beatsToHittest = []
 const beatsToIgnore = []
 
@@ -118,10 +118,10 @@ const hihat = new HiHat(aCtx)
 // zzfx() - the universal entry point -- returns a AudioBufferSourceNode
 const zzfx = (...t) => zzfxP(zzfxG(...t))
 // zzfxP() - the sound player -- returns a AudioBufferSourceNode
-const zzfxP = (...t) => { 
+const zzfxP = (...t) => {
   let e = zzfxX.createBufferSource(), f = zzfxX.createBuffer(t.length, t[0].length, zzfxR);
   t.map((d, i) => f.getChannelData(i).set(d)), e.buffer = f, e.connect(zzfxX.destination);
-  return e 
+  return e
 }
 // zzfxG() - the sound generator -- returns an array of sample data
 const zzfxG = (a = 1, t = .05, h = 220, M = 0, n = 0, s = .1, i = 0, r = 1, o = 0, z = 0, e = 0, f = 0, m = 0, x = 0, b = 0, d = 0, u = 0, c = 1, G = 0, I = zzfxR, P = 99 + M * I, V = n * I, g = s * I, j = G * I, k = u * I, l = 2 * Math.PI, p = (a => 0 < a ? 1 : -1), q = P + j + V + g + k, v = (o *= 500 * l / I ** 2), w = (h *= (1 + 2 * t * Math.random() - t) * l / I), y = p(b) * l / 4, A = 0, B = 0, C = 0, D = 0, E = 0, F = 0, H = 1, J = []) => { for (; C < q; J[C++] = F)++E > 100 * d && (E = 0, F = A * h * Math.sin(B * b * l / I - y), F = p(F = i ? 1 < i ? 2 < i ? 3 < i ? Math.sin((F % l) ** 3) : Math.max(Math.min(Math.tan(F), 1), -1) : 1 - (2 * F / l % 2 + 2) % 2 : 1 - 4 * Math.abs(Math.round(F / l) - F / l) : Math.sin(F)) * Math.abs(F) ** r * a * zzfxV * (C < P ? C / P : C < P + j ? 1 - (C - P) / j * (1 - c) : C < P + j + V ? c : C < q - k ? (q - C - k) / g * c : 0), F = k ? F / 2 + (k > C ? 0 : (C < q - k ? 1 : (C - q) / k) * J[C - k | 0] / 2) : F), A += 1 - x + 1e9 * (Math.sin(C) + 1) % 2 * x, B += 1 - x + 1e9 * (Math.sin(C) ** 2 + 1) % 2 * x, h += o += 500 * z * l / I ** 3, H && ++H > f * I && (h += e * l / I, w += e * l / I, H = 0), m && ++D > m * I && (h = w, o = v, D = 1, H = H || 1); return J };
@@ -136,47 +136,47 @@ const zzfxM = (f, n, o, t = 125) => { let z, e, l, r, g, h, x, a, u, c, d, i, m,
 
 const getSong0 = () => (
   [
-  [
-    [1, 0, 50],
-    [1, 0, 100],
-    [1, 0, 150],
-    [1, 0, 200],
-    [1, 0, 250],
-    [1, 0, 300],
-    [1, 0, 350],
-    [1, 0, 400],
-  ],
-  [
-    // Keep 3 beats before first notes to align this with the metronome...
-    // Seems to give the best results.
     [
-      [7, 0, 21, 21, 24, 21, 26, 21, 28, 26, 24, 24, 28, 24, 31, 24, 28, 24],
-      [3, 0, 0, 9, 33, 12, 33, 14, 9, 40, 14, 36, 12, 40, 12, 43, 12, 36],
-      // [5, 0, 21, 21, 24, 21, 26, 21, 28, 26, 24, 24, 28, 24, 31, 24, 28, 24],
+      [1, 0, 50],
+      [1, 0, 100],
+      [1, 0, 150],
+      [1, 0, 200],
+      [1, 0, 250],
+      [1, 0, 300],
+      [1, 0, 350],
+      [1, 0, 400],
     ],
     [
-      [7, 0, 19, 19, 23, 19, 26, 19, 26, 24, 17, 17, 21, 17, 24, 17, 24, 23],
-      [7, 0, 0, 7, 7, 11, 7, 14, 7, 14, 12, 5, 5, 9, 5, 12, 5, 12, 11],
-      // [5, 0, 21, 21, 24, 21, 26, 21, 28, 26, 24, 24, 28, 24, 31, 24, 28, 24],
-      // [5, 0, 11, 11, 14, 11, 16, 11, 18, 16, 14, 14, 18, 14, 21, 14, 18, 14],
-      // [4, 0, , 11, 11, 14, 11, 16, 11, 18, 16, 14, 14, 18, 14, 21, 14, 18],
+      // Keep 3 beats before first notes to align this with the metronome...
+      // Seems to give the best results.
+      [
+        [7, 0, 21, 21, 24, 21, 26, 21, 28, 26, 24, 24, 28, 24, 31, 24, 28, 24],
+        [3, 0, 0, 9, 33, 12, 33, 14, 9, 40, 14, 36, 12, 40, 12, 43, 12, 36],
+        // [5, 0, 21, 21, 24, 21, 26, 21, 28, 26, 24, 24, 28, 24, 31, 24, 28, 24],
+      ],
+      [
+        [7, 0, 19, 19, 23, 19, 26, 19, 26, 24, 17, 17, 21, 17, 24, 17, 24, 23],
+        [7, 0, 0, 7, 7, 11, 7, 14, 7, 14, 12, 5, 5, 9, 5, 12, 5, 12, 11],
+        // [5, 0, 21, 21, 24, 21, 26, 21, 28, 26, 24, 24, 28, 24, 31, 24, 28, 24],
+        // [5, 0, 11, 11, 14, 11, 16, 11, 18, 16, 14, 14, 18, 14, 21, 14, 18, 14],
+        // [4, 0, , 11, 11, 14, 11, 16, 11, 18, 16, 14, 14, 18, 14, 21, 14, 18],
+      ],
+      [
+        [7, 0, 21, 21, 24, 21, 26, 21, 28, 26, 24, 24, 28, 24, 31, 24, 28, 24],
+        [3, 0, 0, 9, 33, 12, 33, 14, 9, 40, 14, 36, 12, 40, 12, 43, 12, 36],
+        // [5, 0, 21, 21, 24, 21, 26, 21, 28, 26, 24, 24, 28, 24, 31, 24, 28, 24],
+      ],
     ],
     [
-      [7, 0, 21, 21, 24, 21, 26, 21, 28, 26, 24, 24, 28, 24, 31, 24, 28, 24],
-      [3, 0, 0, 9, 33, 12, 33, 14, 9, 40, 14, 36, 12, 40, 12, 43, 12, 36],
-      // [5, 0, 21, 21, 24, 21, 26, 21, 28, 26, 24, 24, 28, 24, 31, 24, 28, 24],
+      0,
+      1,
+      2,
+      1,
+      2,
+      1
     ],
-  ],
-  [
-    0,
-    1,
-    2,
-    1,
-    2,
-    1
-  ],
-  60,
-])
+    60,
+  ])
 // hey ya
 const getSong1 = () => (
   [
@@ -192,22 +192,22 @@ const getSong1 = () => (
     ],
     [
       [
-        [7, 0, ,,, 16, 16, 14, 12, 11, , 11, 11, 12, 11, 11, 9, 7, 9, 11, 11],
-        [7, 0, ,,, , 4, 2, 0, -1, , -1, -1, 0, -1, -1, -3, -5, -3, -1, -1],
+        [7, 0, , , , 16, 16, 14, 12, 11, , 11, 11, 12, 11, 11, 9, 7, 9, 11, 11],
+        [7, 0, , , , , 4, 2, 0, -1, , -1, -1, 0, -1, -1, -3, -5, -3, -1, -1],
         // [3, 0, ,,, 0, 9, 33, 12, 33, 14, 9, 40, 14, 36, 12, 40, 12, 43, 12, 36],
         // [5, 0, 21, 21, 24, 21, 26, 21, 28, 26, 24, 24, 28, 24, 31, 24, 28, 24],
       ],
       [
-        [7, 0, 12, 11, 11, 9, 7, 9, 11, , 12, 12, 11, 11, 11, 11, , ],
-        [7, 0, , 0, -1, -1, -3, -5, -3, -1, , 0, 0, -1, -1, -1, -1, ],
+        [7, 0, 12, 11, 11, 9, 7, 9, 11, , 12, 12, 11, 11, 11, 11, ,],
+        [7, 0, , 0, -1, -1, -3, -5, -3, -1, , 0, 0, -1, -1, -1, -1,],
         // [7, 0, 0, 7, 7, 11, 7, 14, 7, 14, 12, 5, 5, 9, 5, 12, 5, 12, 11],
         // [5, 0, 21, 21, 24, 21, 26, 21, 28, 26, 24, 24, 28, 24, 31, 24, 28, 24],
         // [5, 0, 11, 11, 14, 11, 16, 11, 18, 16, 14, 14, 18, 14, 21, 14, 18, 14],
         // [4, 0, , 11, 11, 14, 11, 16, 11, 18, 16, 14, 14, 18, 14, 21, 14, 18],
       ],
       [
-        [7, 0, ,,, 16, 16, 14, 12, 11, , 11, 11, 12, 11, , 9, 7, 9, 11, 11],
-        [7, 0, ,,, , 4, 2, 0, -1, , -1, -1, 0, -1, -1, -2, -4, -2, -1, -1],
+        [7, 0, , , , 16, 16, 14, 12, 11, , 11, 11, 12, 11, , 9, 7, 9, 11, 11],
+        [7, 0, , , , , 4, 2, 0, -1, , -1, -1, 0, -1, -1, -2, -4, -2, -1, -1],
         // [3, 0, 0, 9, 33, 12, 33, 14, 9, 40, 14, 36, 12, 40, 12, 43, 12, 36],
         // [5, 0, 21, 21, 24, 21, 26, 21, 28, 26, 24, 24, 28, 24, 31, 24, 28, 24],
       ],
@@ -219,20 +219,24 @@ const getSong1 = () => (
     60,
   ]
 )
-let song1, song2
-let songData1, songData2
-let songAudio1, songAudio2
+let songAudio
+let audioStarted
+function getNewSong(i) {
+  let s = i===1 ? getSong0() : getSong1()
+  let sD = zzfxM(...s)
+  return zzfxP(...sD)
+}
+songAudio = getNewSong(1)
 
-let started
-song1 = getSong0()
-songData1 = zzfxM(...song1)
-songAudio1 = zzfxP(...songData1)
-song2 = getSong1()
-songData2 = zzfxM(...song2)
-songAudio2 = zzfxP(...songData2)
+// song1 = getSong0()
+// songData1 = zzfxM(...song1)
+// songAudio1 = zzfxP(...songData1)
+// song2 = getSong1()
+// songData2 = zzfxM(...song2)
+// songAudio2 = zzfxP(...songData2)
 /**/
-  // songAudio = zzfxP(aCtx.currentTime + 5, ...songData)
-  // songAudio.start()
+// songAudio = zzfxP(aCtx.currentTime + 5, ...songData)
+// songAudio.start()
 /* eslint-enable */
 /* #endregion */
 
@@ -330,6 +334,19 @@ function getBeatImage(fillColor, strokeColor, showDebug) {
 function setCurrentLevel(i) {
   currentLevel = i
   clearMusicTrackers()
+  resetAllAssets()
+  songAudio = getNewSong(1)
+  if (i > 0) {
+    songAudio.start()
+  }
+}
+
+function resetAllAssets() {
+  context.clearRect(0, 0, canvas.width, canvas.height)
+  beatsInTransit = []
+  if (audioStarted) {
+    songAudio.stop()
+  }
 }
 
 function parseLevels() {
@@ -459,6 +476,9 @@ const gl = () => GameLoop({
         facilitateCurrentLevel()
         // doAudioStuff()
         break
+      case scenes.gamescene:
+        facilitateCurrentLevel()
+        break
       default:
         break
     }
@@ -523,23 +543,23 @@ function scheduleNote(beatNumber, time) {
   // }
   console.log('scheduling...')
 
-  if (!started && audioReady === undefined) {
+  if (!audioStarted && audioReady === undefined) {
     audioReady = false
   }
-  else if (!started && audioReady === false) {
+  else if (!audioStarted && audioReady === false) {
     audioReady = true
   }
-  else if (!started && audioReady === true) {
+  else if (!audioStarted && audioReady === true) {
     // If this is the beginning of the audio, just set these together.
     // Otherwise it seems like odd things happen when it tries to catch up
     nextNoteTime = aCtx.currentTime
     // Trigger the initial play.
-    songAudio1.start(time)
+    songAudio.start(time)
     // snare.trigger(time)
-    started = true
+    audioStarted = true
   }
 
-  if (started) {
+  if (audioStarted) {
     checkForLevelSpawns()
   }
 
@@ -903,7 +923,7 @@ function initScenes() {
       this.children[0].text = introStrings[5]
       this.children[0].color = COLORS.bad
       this.children[2].text = 'START\n[ space ]',
-      this.children[0].opacity = 0
+        this.children[0].opacity = 0
       this.children[2].opacity = 0
       this.children[1].y = 350
       this.children[1].opacity = 0
